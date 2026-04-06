f96c76ee419cfd32d1a8a654aba9f2a53dcace03
kevinbozarth04 <kevinbozarth04@gmail.com>
2026-04-06 16:36:04 -0700

    Add files via upload

---
+++ J1S2 Calculator.py
@@ -0,0 +1,21 @@
+''' 2) Session 2 Prompt: Write a Python program (.py file) that
+prints the sum of two floating point numbers,
+the difference between two integers,
+and the product of a floating point number and an integer.
+In each case, have the program print out the data type of the resulting answer.
+'''
+
+myInt1 = 5
+myInt2 = 10
+myFloat1 = 6.7
+myFloat2 = 0.3
+
+print("Sum:", myFloat1 + myFloat2)
+print("Data type:", type(myFloat1 + myFloat2))
+
+print("Difference:", myInt1 + myInt2)
+print("Data type:", type(myInt1 + myInt2))
+
+print("Product:", myFloat1 + myInt1)
+print("Data type:", type(myFloat1 + myInt1))
+
